9f515063ad91e798139ec28fc177422901b917e5
acs930 <acs930@gmail.com>
2013-04-22 13:16:59 -0300

    Create stackInterface.java

---
+++ stackInterface.java
@@ -0,0 +1,16 @@
+
+public interface stackInterface <T> {
+  
+	public int size();
+	
+	public boolean isEmpty();
+	
+	public T top();
+	
+	public void push(T element);
+	
+	public T pop();
+	
+	public T peek();
+
+}
